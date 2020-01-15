66a432cf83dfd716ad8b946ff77522962863c680
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2020-01-15 12:14:21 -0300

    desenvolvimento laboratorios .net

---
+++ C#/Aspnet/Laboratios/Lab3/Lab3/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab3.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Exemplo de Web Form</h1>
+            <asp:Label runat="server" ID="mensagemLabel"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ C#/Aspnet/Laboratios/Lab4/Lab4/App/WebApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Lab4.App
+{
+    public static class WebApp
+    {
+        private static string ComentarioArquivo
+        {
+            get
+            {
+                return HttpContext.Current.Server.MapPath("~/comentarios.txt");
+            }
+        }
+
+        public static void ComentarioIncluir(string nome, string comentario)
+        {
+            using (var writter = new StreamWriter(ComentarioArquivo, true, Encoding.UTF8))
+            {
+                writter.WriteLine("{0:dd/MM/yyyy} - {0:HH/mm/ss}", DateTime.Now);
+                writter.WriteLine("{0} : {1}\r\n", nome, comentario);
+            }
+        }
+
+        public static string ComentariosObter()
+        {
+            string texto = string.Empty;
+
+            if (File.Exists(ComentarioArquivo))
+            {
+                using (var reader = new StreamReader(ComentarioArquivo))
+                {
+                    texto = reader.ReadToEnd();
+                }
+            }
+
+            return texto;
+        }
+    }
+}
