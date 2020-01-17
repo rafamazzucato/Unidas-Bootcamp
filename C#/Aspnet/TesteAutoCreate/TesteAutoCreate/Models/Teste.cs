392e1473800757ff8c76fb81fe9fbe0b38150ebf
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2020-01-17 14:42:00 -0300

    Criacao Laboratório 6

---
+++ C#/Aspnet/TesteAutoCreate/TesteAutoCreate/Models/Teste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteAutoCreate.Models
+{
+    public class Teste
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
