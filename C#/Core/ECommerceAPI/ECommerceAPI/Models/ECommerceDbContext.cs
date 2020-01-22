7e722bbc32a56209d04ff53e70bc3806e2abf2c0
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2020-01-22 14:59:51 -0300

    Criação API E-Commerce

---
+++ C#/Core/ECommerceAPI/ECommerceAPI/Models/ECommerceDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Models
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) 
+            : base(options)
+        {
+
+        }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>().ToTable("Produto");
+        }
+    }
+}
---
+++ C#/Core/ECommerceAPI/ECommerceAPI/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required] 
+        public decimal? Preco { get; set; }
+        [Required] 
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataModificacao { get; set; }
+    }
+}
